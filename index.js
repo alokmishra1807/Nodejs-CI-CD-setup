df90db9666aacb697f09c4d7806e7a55b0802252
Alok Mishra <162608167+alokmishra1807@users.noreply.github.com>
2026-09-18 15:15:10 +0530

    Update webhook test message in index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ mongoose
 
 
 app.get("/", (req, res) => {
-    res.send("Hello World: This is to test the webhook between github and Jenkins retry 1");
+    res.send("Hello World: This is to test the webhook between github and Jenkins retry 2");
 });
 
 
